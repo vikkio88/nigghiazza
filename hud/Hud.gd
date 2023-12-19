dcf5e519ce03329ef9b119d08eec0419096b78e1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-12-19 19:54:43 +0000

    adding stamina and health bars

--- hud/Hud.gd
+++ hud/Hud.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 # world interaction
 @onready var action_key:Label = $action_key
 @onready var interaction:Label = $action_key/interaction
+@onready var hp_bar: ProgressBar = $hp_bar
+@onready var stamina_bar: ProgressBar = $stamina_bar
+
 var can_interact = false
 var intereactable_callback: Callable = func(): pass
 
@@ -11,6 +14,7 @@ var intereactable_callback: Callable = func(): pass
 func _ready() -> void:
 	EventBus.interactable_on.connect(self._on_interactable)
 	EventBus.interactable_off.connect(self._off_interactable)
+	EventBus.player_health_update.connect(self._on_player_health_update)
 
 
 func _process(delta: float) -> void:
@@ -30,4 +34,9 @@ func _off_interactable() -> void:
 	action_key.visible = false
 	can_interact = false
 	intereactable_callback = func(): pass
+
+
+func _on_player_health_update(health: int, max_health: int, stamina: int, max_stamina: int) -> void:
+	hp_bar.set_value(health)
+	stamina_bar.set_value(stamina)
 
